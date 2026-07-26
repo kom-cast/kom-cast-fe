6d9b868351de626c3f036d3a0fff6ca5ac242625
정서영 <diyung530@gmail.com>
2026-07-26 15:57:59 +0900

    feat: /portfolio 라우팅 연결

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -3,5 +3,7 @@ export { OnboardingFlow } from './OnboardingFlow'
 
 export { HomePage } from './HomePage'
 export { PlayerPage } from './PlayerPage'
+export { MyStocksPage } from './MyStocksPage'
+export { LibraryPage } from './LibraryPage'
 
 export { NotFoundPage } from './NotFoundPage'
