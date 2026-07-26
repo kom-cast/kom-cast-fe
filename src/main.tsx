0e8e4d5e43fc0d55b3e477e59eb257a1859de8cf
정서영 <diyung530@gmail.com>
2026-07-26 15:11:39 +0900

    chore: react-query QueryClientProvider 연결

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import { StrictMode } from 'react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 
@@ -6,10 +7,14 @@ import './index.css'
 
 import App from './App.tsx'
 
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>,
 )
